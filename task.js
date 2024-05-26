6120b62bdbdfe1e39c0953ef0ddebf78ac020a6e
AKASH021001-0202 <nakashn007@gmail.com>
2024-05-26 17:00:13 +0530

    second

--- task.js
+++ task.js
@@ -5,20 +5,13 @@ db.product.find({});
 
 // 2.Find the product price which are between 400 to 800
 
-db.product.find({product_price:{$gte :400 ,$lte :800}});
+db.product.find({product_price:{$gt :400 ,$lt :800}});
 
 // 3.Find the product price which are not between 400 to 600
-db.product.find({
-    product_price: {
-      $not: {
-        $gte: 400,
-        $lte: 600
-      }
-    }
-  });
+db.product.find({product_price: {$gt: 400, $lt: 600 }});
 
 // 4.List the four product which are greater than 500 in price 
-db.product.find({product_price :{$gte :500}},{},{limit : 4});
+db.product.find({product_price :{$gt :500}},{},{limit : 4});
 
 // 5.Find the product name and product material of each products
 db.product.find({},{product_name :1 ,product_material :1})
